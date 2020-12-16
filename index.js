6c91c3f9cbaa595d113a478fe4c315b0ca6072f7
escohen115 <escohen115@gmail.com>
2020-12-15 19:13:51 -0500

    commit tree 0ac6a0c04a74a0660dc15dcbba862775f5da3f5b

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+var driver = {}
+
+function updateDriverWithKeyAndValue(obj, key, value){
+    const newDriver = {...obj}
+    newDriver[key] = value
+    return newDriver
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(obj, key, value){
+    obj[key] = value
+    return obj
+}
+
+function deleteFromDriverByKey(obj, key){
+    let newObj = {...obj}
+    delete newObj[key]
+    return newObj
+}
+
+
+function destructivelyDeleteFromDriverByKey(obj, key){
+    delete obj[key]
+    return obj
+}
